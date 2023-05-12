f9d7f4aa833b97a35323f4b3343ee1512dbef9bb
DuienkoDmytro <119892965+DuienkoDmytro@users.noreply.github.com>
2023-05-12 19:24:40 +0300

    task_1_resolved

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,17 +5,22 @@ let startColor = null;
 startBtn.addEventListener("click", onClickStartBtn);
 stopBtn.addEventListener("click", onClickStopBtn);
 
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 };
 
 
 function onClickStartBtn() {
-  startColor = setInterval(getRandomHexColor(), 1000);
-  const color = getRandomHexColor(); 
-  document.querySelector('body').style.backgroundColor = `${color}`;
+  startColor = setInterval(changeColorFunction, 1000);
     startBtn.disabled = true;
     stopBtn.disabled = false;
+};
+
+function changeColorFunction() {
+   const color = getRandomHexColor(); 
+  document.querySelector('body').style.backgroundColor = `${color}`;
 };
 
 
