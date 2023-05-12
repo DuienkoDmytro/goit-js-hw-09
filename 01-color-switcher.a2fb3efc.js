0ae8043a3b93087839d766591482a026645e4cb1
DuienkoDmytro <119892965+DuienkoDmytro@users.noreply.github.com>
2023-05-12 16:25:04 +0000

    Deploying to gh-pages from @ DuienkoDmytro/goit-js-hw-09@f9d7f4aa833b97a35323f4b3343ee1512dbef9bb 🚀

--- 01-color-switcher.a2fb3efc.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;function n(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){a=setInterval(n(),1e3);var r=n();document.querySelector("body").style.backgroundColor="".concat(r),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.a2fb3efc.js.map
