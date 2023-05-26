6f0ee749f9db131f9417d46a43509420e6b5b0b9
DuienkoDmytro <119892965+DuienkoDmytro@users.noreply.github.com>
2023-05-26 19:22:13 +0300

    task9_3question

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,61 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector("form");
+const formData = {};
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, rejected) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    } else
+    {
+      rejected(`❌ Rejected promise ${position} in ${delay}ms`)
+    }
+  })
+  promise.then(result => {
+      Notiflix.Notify.success(result);
+  },
+    error => {
+    Notiflix.Notify.failure(error);
+    }
+  );
+};
+
+function submit(e) {
+  e.preventDefault();
+
+  const getItems = JSON.parse(localStorage.getItem('formData'));
+  let position = 1;
+
+  setTimeout(() => {
+    if (getItems.amount < 1) {
+      return
+    }
+
+    createPromise(position, getItems.delay);
+    if (getItems.amount < 1) {
+      return
+    }
+
+    const intervalId = setInterval(() => {
+      position ++;
+
+      createPromise(position, getItems.step);
+
+      if (getItems.amount === position) {
+        clearInterval(intervalId);
+      }
+    }, formData.step);
+  }, formData.delay);
 }
+
+function onInputCreate(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+};
+
+form.addEventListener('submit', submit);
+form.addEventListener('input', onInputCreate);
