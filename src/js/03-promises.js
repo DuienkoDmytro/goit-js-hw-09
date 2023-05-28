136549b77bf4a65394ed872020ea138286ed9b58
DuienkoDmytro <119892965+DuienkoDmytro@users.noreply.github.com>
2023-05-28 14:40:48 +0300

    task3_onemorequestion

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,10 +3,28 @@ import Notiflix from 'notiflix';
 const form = document.querySelector("form");
 const formData = {};
 
-function createPromise(position, delay) {
+// function createPromise(position, delay) {
+//   const promise = new Promise((resolve, rejected) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     if (shouldResolve) {
+//       resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+//     } else
+//     {
+//       rejected(`❌ Rejected promise ${position} in ${delay}ms`)
+//     }
+//   })
+//   promise.then(result => {
+//       Notiflix.Notify.success(result);
+//   },
+//     error => {
+//     Notiflix.Notify.failure(error);
+//     }
+//   );
+// };
 
-  
-  const promise = new Promise((resolve, rejected) => {
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
     setTimeout(() => {
@@ -16,29 +34,47 @@ function createPromise(position, delay) {
         reject({ position, delay });
       }
     }, delay);
-  });
-
-    promise.then(result => {
+  })
+  .then(result => {
       Notiflix.Notify.success(result);
-    },
-      error => {
-        Notiflix.Notify.failure(error);
-      }
-    );
-  }
-const getItems = JSON.parse(localStorage.getItem('formData'));
-let position = 1;
- 
-function submit(e) {
-  e.preventDefault();  
+  },
+    error => {
+    Notiflix.Notify.failure(error);
+    }
+  );
 }
 
-for (let i = 1; i <= )
-function onInputCreate(e) {
-  formData[e.target.name] = e.target.value;
 
-  localStorage.setItem('formData', JSON.stringify(formData));
-};
+function submit(e) {
+  e.preventDefault();
+  const { delay, step, amount } = e.currentTarget.elements;
+  for (let i = 1; i <= amount.value; i++) {
+   
+      
+    
+    if (amount < 1) {
+      createPromise(delay)
+      return
+    }
+   
+
+    
+
+    if (amount > 1) {
+      createPromise(step, delay);
+      clearInterval(intervalId);
+    }
+      
+  }
+}
+
+// function onInputCreate(e) {
+//   formData[e.target.name] = e.target.value;
+
+//   localStorage.setItem('formData', JSON.stringify(formData));
+// };
 
 form.addEventListener('submit', submit);
-form.addEventListener('input', onInputCreate);
+// form.addEventListener('input', onInputCreate);
+
+
